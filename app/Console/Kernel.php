b2afe7b13ef1667e96c154383e58ce872023b352
lukaj <luka.jeremic@acme.co.me>
2024-06-08 00:13:51 +0200

    configuring scheduler for server

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('fetchStockPrices')->everyMinute();
+        $schedule->command('fetchStockPrices')->everyThirtyMinutes();
     }
 
     /**
